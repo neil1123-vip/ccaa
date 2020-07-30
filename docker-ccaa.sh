149c518cff344e2f56286bc08045c8e1199908f7
neil1123-vip <68708135+neil1123-vip@users.noreply.github.com>
2020-07-30 21:00:27 +0800

    Update docker-ccaa.sh
    v5

--- docker-ccaa.sh
+++ docker-ccaa.sh
@@ -148,8 +148,8 @@ function setting(){
 	#更新tracker
 	sh /etc/ccaa/upbt.sh
 	#安装AriaNg
-	#wget ${ccaa_web_url}
-	cp ccaa-master/ccaa_web /usr/sbin/
+	wget ${ccaa_web_url}
+	cp ccaa_web /usr/sbin/
 	chmod +x /usr/sbin/ccaa_web
 	#启动服务
 	#nohup aria2c --conf-path=/etc/ccaa/aria2.conf > /var/log/aria2.log 2>&1 &
